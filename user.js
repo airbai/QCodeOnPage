8dc384c518c9bb014d0dd1880c6212226ab0d65e
air bai <baipengfei@gmail.com>
2012-09-29 15:51:55 +0800

    init

---
+++ user.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+	$('a').hover(function(){
+		var href = $(this).attr('href');
+		if(href)
+		{
+			$(this).qrcode({
+				width: 128,
+				height: 128,
+				text	: href
+			});	
+		}
+	});
+});
